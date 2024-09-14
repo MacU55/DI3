5534379719daec8ca94baa2ae3debd24608737b7
Maks <cma75cma@gmail.com>
2024-09-14 23:19:01 +0200

    added beans and field 'author'

--- src/main/java/home/book/BeansConfiguration.java
+++ src/main/java/home/book/BeansConfiguration.java
@@ -1,5 +1,8 @@
 package home.book;
 
+import java.util.ArrayList;
+import java.util.List;
+import home.chapter.Chapter;
 import home.title.Title;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -21,6 +24,33 @@ import org.springframework.context.annotation.Configuration;
     @Bean
     public Title chapter2Title(){
         return new Title("Spring framework - Chapter 2");
+    }
+
+    @Bean
+    public Chapter chapter1(){
+        Chapter chapter1 = new Chapter();
+        chapter1.setNumber(1);
+        chapter1.setChapterTitle(chapter1Title());
+        chapter1.setContent("The content of chapter 1 goes here.");
+        return chapter1;
+    }
+
+    @Bean
+    public Chapter chapter2(){
+        return new Chapter(2, chapter2Title(), "The content of chapter 1 goes here.");
+    }
+
+    @Bean
+    public Book springBook(){
+        Book springBook = new Book();
+        springBook.setIsbnValue("ISBN1");
+        springBook.setBookTitle(bookTitle());
+        springBook.setAuthor("Author John");
+        List<Chapter> chapterList = new ArrayList<>();
+        chapterList.add(chapter1());
+        chapterList.add(chapter2());
+        springBook.setChapterList(chapterList);
+        return springBook;
     }
 
 }
--- src/main/java/home/book/Book.java
+++ src/main/java/home/book/Book.java
@@ -7,11 +7,13 @@ import home.title.Title;
 public class Book {
     private String isbnValue;
     private Title bookTitle;
+    private String author;
     private List<Chapter> chapterList;
 
-    public Book(String isbnValue, Title bookTitle, List<Chapter> chapterList) {
+    public Book(String isbnValue, Title bookTitle, String author, List<Chapter> chapterList) {
         this.isbnValue = isbnValue;
         this.bookTitle = bookTitle;
+        this.author = author;
         this.chapterList = chapterList;
     }
 
@@ -32,6 +34,14 @@ public class Book {
 
     public void setBookTitle(Title bookTitle) {
         this.bookTitle = bookTitle;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
     }
 
     public List<Chapter> getChapterList() {
